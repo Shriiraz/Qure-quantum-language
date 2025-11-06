6f8e8f0592ec54b039562d8c905d2cff87441ecd
Shriiraz Kukatla <shriiraz@lolMBP.local>
2025-11-06 16:55:54 +0530

    Added semantics.hpp

---
+++ include/semantics.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ast.hpp"
+#include "symbol_table.hpp"
+
+class SemanticAnalyzer {
+private:
+    SymbolTable symTable;
+    bool hasError;
+
+    // ERROR REPORTING: Now takes an ASTNode to extract line/col
+    void error(ASTNode* node, const std::string& msg);
+    
+    // Core traversal methods
+    void checkProgram(Program* node);
+    void checkStatement(Statement* stmt);
+    void checkBlock(Block* block);
+    void checkVarDecl(VarDecl* decl);
+    void checkFuncDecl(FuncDecl* decl);
+
+    DataType checkExpression(Expression* expr);
+    
+    DataType checkBinary(BinaryExpr* expr);
+    DataType checkMethodCall(MethodCallExpr* expr);
+    DataType checkIdentifier(Identifier* expr);
+    DataType checkCall(CallExpr* expr);
+    
+    // Helpers
+    bool isQuantumType(DataType t);
+    bool isNumber(DataType t);
+    std::string typeToString(DataType t);
+
+public:
+    SemanticAnalyzer();
+    bool analyze(Program* root);
+};
